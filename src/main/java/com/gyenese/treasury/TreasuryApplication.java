ed7bdde8c4a7be6409ef1f9dff98b09fce866000
tamasgyenese <gyenesetomi@gmail.com>
2023-10-06 17:19:24 +0200

    Adding base setup and to be able to run the spring api

---
+++ src/main/java/com/gyenese/treasury/TreasuryApplication.java
@@ -0,0 +1,12 @@
+package com.gyenese.treasury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasuryApplication.class, args);
+    }
+}
